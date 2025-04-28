8a98539a85b6ca73113e386d0a6c633329a58168
leticiaArauj0 <leticia.araujo2718@gmail.com>
2025-04-28 11:10:56 -0400

    back-end fonus

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const USERS_FILE = "users.json";
+
+const readUsers = () => {
+    if (!fs.existsSync(USERS_FILE)) return [];
+    return JSON.parse(fs.readFileSync(USERS_FILE, "utf8"));
+};
+
+const saveUsers = (users) => {
+    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
+};
+
+app.post("/register", (req, res) => {
+    const users = readUsers();
+    const { name, age, email, password } = req.body;
+
+    if (users.find(u => u.email === email)) {
+        return res.status(400).json({ message: "Usuário já existe" });
+    }
+
+    users.push({ name, age, email, password });
+    saveUsers(users);
+
+    res.status(200).json({ user: true})
+});
+
+app.post("/login", (req, res) => {
+    const users = readUsers();
+    const { email, password } = req.body;
+
+    const user = users.find(u => u.email === email && u.password === password);
+
+    if (!user) {
+        return res.status(400).json({ message: "E-mail e/ou senha inválida" });
+    }
+
+    res.status(200).json({ user });
+});
+
+app.listen(PORT, "0.0.0.0", () =>
+    console.log(`Servidor rodando em http://0.0.0.0:${PORT}`)
+);
